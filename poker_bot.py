69a83769467aaf1726c6b406bbf1107a6cc04564
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2020-07-05 20:22:07 +0200

    txt reco

--- poker_bot.py
+++ poker_bot.py
@@ -6,19 +6,38 @@ pic = pyautogui.screenshot()
 # Save the image
 pic.save('screenshot.png')
 #pyautogui.alert('This displays some text with an OK button.')
-card = 'images/cards/6a.png'
-if pyautogui.locateOnScreen(card):
-    pyautogui.click(card)
+card = 'images/cards/8h.png'
+#if pyautogui.locateOnScreen(card) != None:
+#    pyautogui.click(card)
 
-bet = '5'
-action = 3
-if pyautogui.locateOnScreen('images/check.png') != None and action == 0:
-    pyautogui.click('images/check.png')
-if pyautogui.locateOnScreen('images/fold.png') != None and action == 1:
-    pyautogui.click('images/fold.png')
-if pyautogui.locateOnScreen('images/bet.png') != None and action == 2:
-    pyautogui.click('images/bet.png')
-    pyautogui.write(bet, interval=0.25)
-    pyautogui.press('enter')
 
-#pyautogui.click(50,50)
+#----read data----
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+img = Image.open('images/test6.png').convert('LA')
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(2)
+img.save('images/test/greyscale2.png')
+
+print(pytesseract.image_to_string('images/test/greyscale2.png'))
+
+
+#----nb players----
+#nb = 0
+#for pos in pyautogui.locateAllOnScreen('images/nplayer.png'):
+#	nb += 1
+#print(nb)
+
+
+#----actions----
+#bet = '5'
+#action = 1
+#if pyautogui.locateOnScreen('images/check.png') != None and action == 0:
+#    pyautogui.click('images/check.png')
+#if pyautogui.locateOnScreen('images/fold.png') != None and action == 1:
+#    pyautogui.click('images/fold.png')
+#if pyautogui.locateOnScreen('images/bet.png') != None and action == 2:
+#    pyautogui.click('images/bet.png')
+#    pyautogui.write(bet, interval=0.25)
+#    pyautogui.press('enter')
